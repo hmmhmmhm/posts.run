603094249d028fd667a26086370f1eb1734f55dd
hmmhmmhm <hmmhmmhm@naver.com>
2023-12-31 06:06:42 +0900

    feat: Add transparent background

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { CircularProgress } from "@mui/material";
 import Spline, { SplineEvent } from "@splinetool/react-spline";
 import { Application as SplineApplication } from "@splinetool/runtime";
 
 export default function App() {
   const splineRef = useRef<SplineApplication | null>(null);
+  const [isSplineLoaded, setIsSplineLoaded] = useState(false);
+
   const onLoad = (spline: SplineApplication) => {
     if (spline) splineRef.current = spline;
   };
@@ -13,31 +15,36 @@ export default function App() {
   const onMouseUp = (event: SplineEvent) => {
     console.log({ targetName: event.target.name, id: event.target.id });
     if (event.target.name === "Door wrap") {
-      console.log("문 클릭 시도");
-      // TODO 이미지 표시 활성화
+      setIsSplineLoaded(true);
     }
     if (event.target.name === "User Message") {
       console.log("유저 메세지 수정 시도");
       splineRef.current?.setVariable("User Message", "테스트1");
     }
-    if (event.target.name === "User A.I Message") {
-      console.log("유저 A.I 메세지 수정 시도");
-      splineRef.current?.setVariable("AI Message Guide", "테스트2");
+    if (event.target.name === "To Ment") {
+      console.log("수신자 변경 시도");
+      splineRef.current?.setVariable("To Ment", "테스트3");
     }
     if (event.target.name === "Share OK Text") {
       console.log("공유 기능 동작");
       // TODO 공유 native 기능 동작
     }
-    if (event.target.name === "To Ment") {
-      console.log("수신자 변경 시도");
-      splineRef.current?.setVariable("To Ment", "테스트3");
-    }
   };
 
   return (
-    <main className="bg-[#E8C9A4] size-full h-screen">
+    <main className="bg-[#E8C9A4] size-full h-screen overflow-hidden">
       <div className="absolute size-full h-screen flex justify-center items-center">
-        <CircularProgress className="text-white" size={64} />
+        {!isSplineLoaded && (
+          <CircularProgress className="text-white" size={64} />
+        )}
+        {isSplineLoaded && (
+          <div
+            className="size-full bg-cover bg-center transition-opacity duration-1000 ease-in opacity-0 animate-[fadeIn_3s_ease_forwards]"
+            style={{
+              backgroundImage: "url('https://i.imgur.com/jSym2zE.jpeg')",
+            }}
+          />
+        )}
       </div>
 
       <Spline
