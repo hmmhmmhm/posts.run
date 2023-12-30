fb5e0c2cf9e912b3a6b4fa9116e2e50d3ae6ac6d
hmmhmmhm <hmmhmmhm@naver.com>
2023-12-31 03:42:12 +0900

    feat: Add spline design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <main className=""></main>;
+"use client";
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <main className="bg-[#E8C9A4] size-full">
+      <Spline scene="https://prod.spline.design/49K5A8xapEdTSHlv/scene.splinecode" />
+    </main>
+  );
 }
