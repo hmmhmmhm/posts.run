d4d5c92257082901906d8a9428ea040cc0844f6d
hmmhmmhm <hmmhmmhm@naver.com>
2024-01-06 19:04:20 +0900

    fix: 이미지 업로드 실패시 에러 추적 메세지 심화

--- app/api/new-year/upload-image/route.ts
+++ app/api/new-year/upload-image/route.ts
@@ -6,13 +6,24 @@ export async function POST(request: Request) {
   const formData = await request.formData();
   const file = formData.get("file") as File;
 
-  const uploadedImageUrl = await uploadImageFile({
-    buffer: await file.arrayBuffer(),
-    folder: "new-year-user-images",
-    info: JSON.stringify({
-      name: file.name,
-    }),
-  });
+  try {
+    const uploadedImageUrl = await uploadImageFile({
+      buffer: await file.arrayBuffer(),
+      folder: "new-year-user-images",
+      info: JSON.stringify({
+        name: file.name,
+      }),
+    });
 
-  return Response.json({ uploadedImageUrl });
+    return Response.json({ uploadedImageUrl });
+  } catch (e) {
+    console.error(`Failed to upload image: ${e}`, file);
+    return Response.json(
+      {
+        error:
+          "An error occurred while uploading the image. Please try again later.",
+      },
+      { status: 500 }
+    );
+  }
 }
